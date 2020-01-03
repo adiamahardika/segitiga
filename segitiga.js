138159cd39a1c8a9854f29731b8be51ff1ebd2f1
adiamahardika <adiamahardika.work@gmail.com>
2020-01-03 23:01:01 +0700

    Membuat function segitiga di segitiga.js

---
+++ segitiga.js
@@ -0,0 +1,12 @@
+function segitiga(a) {
+    for (let jumlahBaris = 1; jumlahBaris <= a; jumlahBaris++){
+        for(let jumalahBintang = 1; jumalahBintang <= jumlahBaris; jumalahBintang++){
+            let s = ''
+            s += '^'    
+        }
+        s += '\n'
+    }
+    console.log(s)
+}
+
+segitiga(3)
